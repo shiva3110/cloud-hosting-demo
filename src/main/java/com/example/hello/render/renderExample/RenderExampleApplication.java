bdab158ced541ceaab5d752eb2ac9e32ef90c232
shiva3110 <58117564+shiva3110@users.noreply.github.com>
2024-04-09 18:17:45 +0530

    initial project lines

---
+++ src/main/java/com/example/hello/render/renderExample/RenderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello.render.renderExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderExampleApplication.class, args);
+	}
+
+}
